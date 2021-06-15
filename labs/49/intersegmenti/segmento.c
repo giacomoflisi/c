52e4b56db13a42437eb8c92ac0ea9929f360ca36
giacomoflisi <giacomoflisi.jack@gmail.com>
2021-06-15 22:54:40 +0200

    exam 49 es3

--- labs/49/intersegmenti/segmento.c
+++ labs/49/intersegmenti/segmento.c
@@ -1,4 +1,5 @@
 #include "segmento.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
 
@@ -7,38 +8,42 @@ struct segmento *intersezione(struct segmento s1, struct segmento s2)
     if(s1.a > s1.b || s2.a > s2.b)
         return NULL;
 
-    int tmp = s1.a;
-    int first[10];
-    size_t i = 0, n;
-
-    while(tmp  <= s1.b){
-        first[i] = tmp;
-        tmp++;
-        i++;
-    }
-    n = sizeof(first)/sizeof(int);
-
     struct segmento *inters;
     inters = malloc(sizeof(struct segmento));
-
-    for(i = 0; i<=n; i++){
-        for(tmp = s2.a; tmp<s2.b; tmp++){
-            if(first[i] == tmp){
-                inters->a = tmp;
-
-            }
-        }
+    
+    //se i segmenti sono identici
+    if(s1.a == s2.a && s1.b == s2.b){
+        inters->a = s1.a;
+        inters->b = s1.b;
     }
 
-    for(i = n; i>=0; i--){
-        for(tmp = s2.b; tmp>=s.2a; tmp--){
-            if(first[i] == tmp){
-                inters->b = tmp;
+    //se i segmenti non si intersicano
+    if(s1.b < s2.a || s1.a > s2.b)
+        return NULL;
 
-            }
-        }
+    if(s1.a <= s2.a && s1.b >= s2.a){
+        inters->a = s2.a;
+
+        if(s1.b >= s2.a && s1.b <= s2.b)
+            inters->b = s1.b;
+
+        if(s1.b > s2.b)
+            inters->b = s2.b;
     }
 
+    if(s1.a <= s2.b && s1.a >= s2.a){
+        inters->a = s1.a;
+
+        if(s1.b <= s2.b)
+            inters->b = s1.b;
+
+        if(s1.b > s2.b)
+            inters->b = s2.b;
+    }
+
+
+    printf("inters->a = %i\n", inters->a);
+    printf("inters->b = %i\n", inters->b);
 
     return inters;
 }
